34d56631ba771a3784037aa15bc8407d1f503e1e
Jeff Adkins <jeffadkins1@gmail.com>
2025-12-30 16:33:00 -0700

    fix(performance): Correct cache import path

--- website/app/api/beast-mode/marketplace/plugins/route.ts
+++ website/app/api/beast-mode/marketplace/plugins/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import cache, { cacheKeys, cacheTTL } from '../../../../lib/cache';
+import cache, { cacheKeys, cacheTTL } from '../../../../../lib/cache';
 
 /**
  * BEAST MODE Plugin Registry API
