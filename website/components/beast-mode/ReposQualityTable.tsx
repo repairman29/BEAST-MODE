14355e5d86523be9b41117538d60f48e15863b49
repairman29 <jeffadkins1@gmail.com>
2026-01-07 14:40:51 -0700

    fix: Uncomment variables in ReposQualityTable - build succeeds

--- website/components/beast-mode/ReposQualityTable.tsx
+++ website/components/beast-mode/ReposQualityTable.tsx
@@ -256,6 +256,7 @@ export default function ReposQualityTable({ repos, onRefresh }: ReposQualityTabl
                       // Note: This is client-side processing of data already fetched from API
                       // TODO: Move to API route for better architecture
                       // ARCHITECTURE: Moved to API route
+// // ARCHITECTURE: Moved to API route
 // const reposWithData = Array.from(repoQualities.values())
                         .filter((r: any) => r.quality !== undefined)
                         .map((r: any) => ({
@@ -312,7 +313,6 @@ export default function ReposQualityTable({ repos, onRefresh }: ReposQualityTabl
                       try {
                         // Get average quality
                         // ARCHITECTURE: Moved to API route
-// // ARCHITECTURE: Moved to API route
 // const reposWithQuality = Array.from(repoQualities.values())
                           .filter((r: any) => r.quality !== undefined && r.quality !== null);
                         
